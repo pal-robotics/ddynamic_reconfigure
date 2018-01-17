881b986549df8ba89023b2132b821e1a4bf70cea
Victor Lopez <victor.lopez@pal-robotics.com>
2018-01-17 11:08:32 +0100

    Add header to add compatibility with new version

---
+++ include/ddynamic_reconfigure/ddynamic_reconfigure.h
@@ -0,0 +1,7 @@
+#include <ddynamic_reconfigure/DDynamicReconfigure.h>
+// To make this compatible with newer versions
+
+namespace ddynamic_reconfigure
+{
+    typedef ::DDynamicReconfigure DDynamicReconfigure;
+}
